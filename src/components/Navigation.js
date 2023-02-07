d2284fe827bdaad2c8f48eafeafc529f917d82c9
Utsargo Roy <royutsargo@gmail.com>
2023-02-07 20:58:17 +0600

    Create empty my profile

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,28 +1,26 @@
 import React from 'react';
+import { NavLink, Link } from 'react-router-dom';
 import logo from '../assets/images/planet.png';
 
 const Navigation = () => (
-  <header>
-    <nav className="navigation">
-      <div className="brand">
-        <img className="logo" src={logo} alt="logo" />
-        <span> Space Travelers&apos; Hub</span>
-      </div>
-    </nav>
+  <header className="d-flex flex-column align-center col-md-12">
+    <div className="container">
+      <nav className="navigation d-flex justify-between align-center">
+        <Link className="brand" to="/">
+          <img className="logo" src={logo} alt="logo" />
+          <h3> Space Travelers&apos; Hub</h3>
+        </Link>
+        <ul className="main-nav">
+          <li className="nav-item">
+            <NavLink to="/profile" className={({ isActive }) => (isActive ? 'active' : 'link')}>
+              My Profile
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </div>
   </header>
 
 );
 
 export default Navigation;
-
-// import React from 'react';
-
-// const Navigation = () => {
-//   <header>
-//     <nav className="navigation">
-//       Menu will go here
-//     </nav>
-//   </header>;
-// };
-
-// export default Navigation;
---
+++ src/pages/Profile.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Profile = () => (
+  <main className="d-flex flex-column align-center col-md-12">
+    <div className="container">
+      <h1>My profile</h1>
+    </div>
+  </main>
+
+);
+
+export default Profile;
