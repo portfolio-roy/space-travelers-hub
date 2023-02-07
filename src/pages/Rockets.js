e83ef06185f533b04c85d81dc5d2e1a690c946f0
Utsargo Roy <royutsargo@gmail.com>
2023-02-07 21:33:23 +0600

    Add rockets page to the router

---
+++ src/pages/Rockets.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Rocket from '../components/Rocket';
+
+const Rockets = () => (
+  <main className="d-flex flex-column align-center col-md-12">
+    <div className="container">
+      <Rocket />
+    </div>
+  </main>
+);
+export default Rockets;
