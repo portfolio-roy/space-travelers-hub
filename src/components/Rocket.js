5ab3baed5cfd8547451c01e9723710c7c5a524d4
Utsargo Roy <royutsargo@gmail.com>
2023-02-09 09:20:03 +0600

    Add reserve funtionality to the Rocket component

--- src/components/Rocket.js
+++ src/components/Rocket.js
@@ -1,19 +1,36 @@
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { bookRocket } from '../redux/rockets/rocket';
 
-const Rocket = ({ title, description, image }) => (
-  <div className="rocket d-flex align-start">
-    <img className="rocket-image" src={image} alt={title} />
-    <div className="title-desc">
-      <h3>{title}</h3>
-      <p>{description}</p>
+const Rocket = ({
+  id, title, description, image, reserved, reservedtext,
+}) => {
+  const dispatch = useDispatch();
+  const bookingHandler = () => {
+    dispatch(bookRocket(id));
+  };
+  return (
+    <div className="rocket d-flex align-start">
+      <img className="rocket-image" src={image} alt={title} />
+      <div className="title-desc">
+        <h3>{title}</h3>
+        <p>
+          <span id={reserved ? 'reserved' : 'not-reserved'}>{reservedtext}</span>
+          {description}
+        </p>
+        <button id="booking" type="button" onClick={bookingHandler}>Reserve Rocket</button>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 Rocket.propTypes = {
   title: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
   image: PropTypes.string.isRequired,
+  reserved: PropTypes.bool.isRequired,
+  id: PropTypes.string.isRequired,
+  reservedtext: PropTypes.string.isRequired,
 };
 
 export default Rocket;
